5cb46473fff2d5fd40827bcb53a3f155ff4a63b2
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-05-25 16:59:53 -0400

    Check every folder in directory

--- albumOrganizer.py
+++ albumOrganizer.py
@@ -2,38 +2,37 @@ import os
 import shutil
 
 def move_single_link_files(source_folder):
-    destination_folder = os.path.join(source_folder, 'justOnce')
-    # Create the destination folder if it doesn't exist
-    if not os.path.exists(destination_folder):
-        os.makedirs(destination_folder)
+    # Recursively search through the folders
+    for root, dirs, files in os.walk(source_folder):
+        print(root)
+        destination_folder = os.path.join(root, 'justOnce')
+        # Create the destination folder if it doesn't exist
+        if not os.path.exists(destination_folder):
+            os.makedirs(destination_folder)
 
-    # Get a list of all files in the source folder
-    file_list = os.listdir(source_folder)
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
 
-    # Iterate over each file in the source folder
-    for file_name in file_list:
-        file_path = os.path.join(source_folder, file_name)
+            # Check if the file is a regular file
+            if os.path.isfile(file_path) and file_name.endswith('.txt'):
+                # Open the file and read its contents
+                with open(file_path, 'r') as file:
+                    lines = file.readlines()
 
-        # Check if the file is a regular file
-        if os.path.isfile(file_path) and file_name.endswith('.txt'):
-            # Open the file and read its contents
-            with open(file_path, 'r') as file:
-                lines = file.readlines()
+                # Check the number of lines in the file
+                if len(lines) == 1:
+                    # Move the .txt file to the destination folder
+                    shutil.move(file_path, os.path.join(destination_folder, file_name))
 
-            # Check the number of lines in the file
-            if len(lines) == 1:
-                # Move the relevant files    to the destination folder
-                shutil.move(file_path, os.path.join(destination_folder, file_name))
+                    # Move the corresponding image file if it exists
+                    image_file = os.path.splitext(file_path)[0] + ' first_image.jpg'
+                    if os.path.exists(image_file):
+                        shutil.move(image_file, os.path.join(destination_folder, os.path.basename(image_file)))
 
-
-                # Move the corresponding image file if it exists
-                image_file = os.path.splitext(file_path)[0] + ' first_image.jpg' 
-                if os.path.exists(image_file):
-                    shutil.move(image_file, os.path.join(destination_folder, os.path.basename(image_file)))
-                    
-                image_file = os.path.splitext(file_path)[0] + '.jpg'  
-                if os.path.exists(image_file):
-                    shutil.move(image_file, os.path.join(destination_folder, os.path.basename(image_file)))
+                    # Move the corresponding image file if it exists
+                    image_file = os.path.splitext(file_path)[0] + '.jpg'  # Assuming the images have .jpg extension
+                    if os.path.exists(image_file):
+                        shutil.move(image_file, os.path.join(destination_folder, os.path.basename(image_file)))
     print("Success!")
 
 # Usage example
