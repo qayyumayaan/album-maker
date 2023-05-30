44bca5d90f67337921b95e5326a90641778275c0
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-05-30 00:26:11 -0400

    Create coreDependencies.py

---
+++ coreDependencies.py
@@ -0,0 +1,18 @@
+def windows2linux(windows_path):
+    drive, path = windows_path[0], windows_path[3:]
+    path = path.replace("\\", "/")
+    linux_path = "/mnt/{}/{}".format(drive.lower(), path)
+    return linux_path
+
+def linux2windows(linux_path):
+    path_components = linux_path.split("/")
+    drive, path = path_components[2], "/".join(path_components[3:])
+    path = path.replace("/", "\\")
+    windows_path = "{}:\\{}".format(drive.upper(), path)
+    return windows_path
+
+import os
+def isImageFile(filename):
+    image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', ".tiff"]
+    file_ext = os.path.splitext(filename)[1].lower()
+    return file_ext in image_extensions
