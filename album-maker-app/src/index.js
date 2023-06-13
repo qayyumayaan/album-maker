50db09177e25e449e84d8d48d1c78ec74af59fed
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-06-13 15:20:26 -0400

    Add boilerplate

--- album-maker-app/src/index.js
+++ album-maker-app/src/index.js
@@ -7,19 +7,17 @@ if (require('electron-squirrel-startup')) {
 }
 
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
     },
   });
 
-  // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
   mainWindow.webContents.openDevTools();
 };
 
